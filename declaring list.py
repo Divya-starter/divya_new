b140c7ae3bc8f5244bb2164cb1f68378b76c5a22
divya <ithedivya03@gmail.com>
2024-12-28 11:28:20 +0530

    declaring list

---
+++ declaring list.py
@@ -0,0 +1,13 @@
+list1=[2,3,4,2,3,4]
+list1_uni=[]
+list1_dup=[]
+dict1={}
+count=0
+for i in list1:
+    if i not in dict1:
+        dict1[i]=1
+    else:
+        count=dict1[i]
+        dict1[i]=count+1
+for keys,val in dict1.items():
+    print(f"{keys}apperaed of times {val} times")
